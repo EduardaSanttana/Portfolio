91ebc49e81b6e97d616c5aa133c1869144df277e
eduardasanttana <125045346+EduardaSanttana@users.noreply.github.com>
2025-02-10 22:54:25 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,29 @@
+function retrieveRepos() {
+    const user = 'EduardaSanttana';
+    const container = document.getElementById("posts");
+
+    container.insertAdjacentHTML("beforeend", `
+        <div class="post">
+            <a class="postLink" href="https://github.com/EduardaSanttana/TCC-campezan" target="_blank">TCC - Campezan</a>
+        </div>
+    `);
+
+    axios.get(`https://api.github.com/users/${user}/repos`).then((result) => {
+        let { data } = result;
+
+        data.forEach(element => {
+            if (element.name !== "TCC-campezan") { 
+                container.insertAdjacentHTML("beforeend", `
+                    <div class="post">
+                        <a class="postLink" href="${element.html_url}" target="_blank">${element.full_name}</a>
+                    </div>
+                `);
+            }
+        });
+    }).catch(error => console.error("Erro ao buscar repositórios:", error));
+}
+
+function showHome() {
+    document.getElementById("main").style.display = 'grid';
+    document.getElementById("profile").style.display = 'none';
+}
